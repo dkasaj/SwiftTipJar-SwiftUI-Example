b003796156b8691775fb23efd3d958e4634c804c
Daniel Kašaj <dkasaj@ux-first.hr>
2022-04-13 20:10:21 +0200

    Initial Commit

---
+++ SwiftTipJarExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftTipJarExample
+//
+//  Created by Daniel Kasaj on 13.04.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftTipJarExample/SwiftTipJarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTipJarExampleApp.swift
+//  SwiftTipJarExample
+//
+//  Created by Daniel Kasaj on 13.04.2022..
+//
+
+import SwiftUI
+
+@main
+struct SwiftTipJarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
